721b76040ab4cca1979393231809553a2c2892d4
naji <hafidanaji13@gmail.com>
2025-03-27 10:57:46 +0000

    docketr compose yaml kafka

---
+++ src/main/java/net/naji/eventdrivendockercompose_yaml_springcoudstream/EventDrivenDockerComposeYamlSpringcoudStreamApplication.java
@@ -0,0 +1,13 @@
+package net.naji.eventdrivendockercompose_yaml_springcoudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenDockerComposeYamlSpringcoudStreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenDockerComposeYamlSpringcoudStreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/naji/eventdrivendockercompose_yaml_springcoudstream/EventDrivenDockerComposeYamlSpringcoudStreamApplicationTests.java
@@ -0,0 +1,13 @@
+package net.naji.eventdrivendockercompose_yaml_springcoudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenDockerComposeYamlSpringcoudStreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
